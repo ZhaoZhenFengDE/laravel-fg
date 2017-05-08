7a9e94a7d70e77626e552452cce10c5942a786d3
ZZF <zzf950307@163.com>
2017-05-08 14:39:49 +0800

    修改部分前端显示效果

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,13 @@
           content=兰亭花礼苑是一家专注于做网上鲜花礼品的电商网站，致力于将新鲜的鲜花礼品以最快的速度送到用户手中，鲜花礼品网可24小时预订鲜花，同城送花、异地送花服务,网上订花后最快3小时即可将鲜花快递上门,送花网站覆盖中国900多城市！>
     <meta name=keyword content=鲜花,鲜花网,鲜花快递,网上订花送花,鲜花礼品网,鲜花礼品,网上订花,送花网站,兰亭花礼网>
     <link rel="shortcut icon" href=./src/assets/img/favicon.ico>
-    <link href={{asset('/resources/views/static/css/app.0a6a451d7d6a1654511742d14dc45956.css')}} rel=stylesheet>
+    <link href={{asset('/resources/views/static/css/app.19e9626cd4b57b33fa39d952ea1146f2.css')}} rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
-<script type=text/javascript src={{asset('/resources/views/static/js/manifest.964353500e0c5fd92353.js')}}></script>
+<script type=text/javascript src={{asset('/resources/views/static/js/manifest.41c89666d3ea5fcf57e0.js')}}></script>
 <script type=text/javascript
-        src={{asset('/resources/views/static/js/vendor.23f263d7fc3df711f8ce.js')}}></script>
-<script type=text/javascript src={{asset('/resources/views/static/js/app.9202bbe858bc50546f7a.js')}}></script>
+        src={{asset('/resources/views/static/js/vendor.023832eae6fd0b7556ed.js')}}></script>
+<script type=text/javascript src={{asset('/resources/views/static/js/app.a9dfe3d4c39b753d24cb.js')}}></script>
 </body>
 </html>
